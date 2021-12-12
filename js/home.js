ff1c19c1ad17f523091b871dc7fe2ed1bf668369
KFURU6 <90489907+KFURU6@users.noreply.github.com>
2021-12-12 23:33:19 +0900

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,56 @@
+// page reload
+window.onpageshow = function(event){
+    if(event.persisted){
+      window.location.reload();
+    }
+  }
+
+  // Appear from below
+//     const movepage = document.getElementById('body');
+//   window.addEventListener('load', function() {
+//       movepage.style.opacity = '1.0';
+//       movepage.style.transform = 'translateY(0vw)';
+//   })
+
+  // picture changing
+  const swiper = new Swiper('.swiper', {
+      loop: true,
+      speed: 500,
+      autoplay:{
+        delay:2500
+      },
+    
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+
+  // hamberger-menu
+  const hambergerBtn = document.getElementById('ham-btn');
+  const hambergerIcon = document.getElementById('ham-icon');
+  const nav = document.getElementById('nav');
+    hambergerIcon.addEventListener('click', function() {
+      hambergerBtn.classList.toggle('active')
+      hambergerIcon.classList.toggle('active')
+      nav.classList.toggle('active')
+  });
+
+console.log('画面の高さ',window.innerHeight)
+const animationTargetElement = document.querySelectorAll('.animation-image');
+console.log(animationTargetElement);
+window.addEventListener('scroll', function(){
+    for(let i = 0; i <= animationTargetElement.length; i++){
+        const ElementHeight = animationTargetElement[i].getBoundingClientRect().top;
+        console.log(ElementHeight)
+        if(window.innerHeight * .7 > ElementHeight){
+            animationTargetElement[i].classList.add('active');
+            animationTargetElement[i].style.opacity = "1.0";
+            console.log('hello');
+        }
+    }
+})
