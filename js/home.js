33ba180632098bb90ba6dd82dd2f5a3d88c16aaa
KFURU6 <my_time_is_now@i.softbank.jp>
2021-12-16 06:03:27 +0900

    サイトに動きをつけました。

--- js/home.js
+++ js/home.js
@@ -5,25 +5,27 @@ window.onpageshow = function(event){
     }
   }
 
-  // Appear from below
-//     const movepage = document.getElementById('body');
-//   window.addEventListener('load', function() {
-//       movepage.style.opacity = '1.0';
-//       movepage.style.transform = 'translateY(0vw)';
-//   })
+  // message
 
-  // hamberger-menu
-  const hambergerBtn = document.getElementById('ham-btn');
-  const hambergerIcon = document.getElementById('ham-icon');
-  const nav = document.getElementById('nav');
-    hambergerIcon.addEventListener('click', function() {
-      hambergerBtn.classList.toggle('active')
-      hambergerIcon.classList.toggle('active')
-      nav.classList.toggle('active')
-  });
+  const topMessage = document.getElementById.
+  window.addEventListener('load',function() {
+    topMessage.style.fontSize = "16px";
+    topMessage.style.letterSpacing = "8px";
+  })
 
+// hamberger-menu
+const hambergerBtn = document.getElementById('ham-btn');
+const hambergerIcon = document.getElementById('ham-icon');
+const nav = document.getElementById('nav');
+  hambergerIcon.addEventListener('click', function() {
+    hambergerBtn.classList.toggle('active')
+    hambergerIcon.classList.toggle('active')
+    nav.classList.toggle('active')
+});
+
+// move-target
 console.log('画面の高さ',window.innerHeight)
-const animationTargetElement = document.querySelectorAll('.animation-image');
+const animationTargetElement = document.querySelectorAll('.move-target');
 console.log(animationTargetElement);
 window.addEventListener('scroll', function(){
     for(let i = 0; i <= animationTargetElement.length; i++){
@@ -32,6 +34,7 @@ window.addEventListener('scroll', function(){
         if(window.innerHeight * .7 > ElementHeight){
             animationTargetElement[i].classList.add('active');
             animationTargetElement[i].style.opacity = "1.0";
+            animationTargetElement[i].style.transform = "translateY(0)";
             console.log('hello');
         }
     }
